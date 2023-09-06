0d8f7d659e10b1df7bc5f7ff0e407c7a83e74c23
gleamsoda <gleamsoda99@gmail.com>
2023-09-06 20:51:10 +0900

    rm

--- app/articles/[slug]/prose.tsx
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx'
-
-export function Prose({ children, className }: React.ComponentProps<'div'>) {
-  return (
-    <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
-  )
-}
